d0c5dcd27ab43d182f0e379f24b31b849c88ae3d
nathanbelaynehf <nathanbelayneh.1f@gmail.com>
2024-09-24 06:13:30 -0800

    fixed the video icon and the home screen

--- js/script.js
+++ js/script.js
@@ -12,4 +12,16 @@ $(document).ready(function() {
       }
     }, 1300);  // 500ms delay
   });
-  
+  var videoModal = document.getElementById('videoModal');
+  videoModal.addEventListener('show.bs.modal', function (event) {
+    var button = event.relatedTarget; 
+    var videoSrc = button.getAttribute('data-bs-src'); 
+    var autoplaySrc = videoSrc + "?autoplay=1"; // Add autoplay to the URL
+    var video = videoModal.querySelector('iframe');
+    video.src = autoplaySrc;
+  });
+
+  videoModal.addEventListener('hide.bs.modal', function () {
+    var video = videoModal.querySelector('iframe');
+    video.src = ''; // Stop the video when the modal is closed
+  });
